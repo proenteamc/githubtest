06fa816dd317e2bbc4042065c671a12297f2ddff
TeamC <projectde2020.c@gmail.com>
2020-04-30 23:52:35 +0900

    first commit test

--- githubtest/ViewController.swift
+++ githubtest/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var testbt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
